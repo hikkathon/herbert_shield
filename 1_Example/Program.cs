ffa0f2f3b0819f73aceb30d9a268709ddc44bf6f
hikkathon <hikkathon@gmail.com>
2020-03-21 13:38:32 +0700

    up

---
+++ 1_Example/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Это простая программа на C#
+ * Назовем ее Example.cs
+ */
+using System;
+
+namespace _1_Example
+{
+    class Program
+    {
+        // Любая программа на C# начинается с вызова методв Main().
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Простая программа на C#.");
+
+            Console.ReadKey(true);
+        }
+    }
+}
