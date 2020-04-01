6c2a7acfbf11dc9e930b4c94230b385c68e789eb
hikkathon <hikkathon@gmail.com>
2020-04-01 18:00:37 +0700

    up

---
+++ 11_Use_byte/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Использовать тип byte.
+ */
+
+using System;
+
+namespace _11_Use_byte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte x;
+            int sum;
+
+            sum = 0;
+
+            for (x = 1; x <= 100; x++)
+            {
+                sum += x;
+            }
+            Console.WriteLine("Сумма чисел от 1 до 100 равна " + sum);
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ 12_FindRadius/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Определить радиус окружности по площади круга.
+ */
+
+using System;
+
+namespace _12_FindRadius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r;
+            double area;
+
+            area = 10.0;
+
+            r = Math.Sqrt(area / 3.1416);
+
+            Console.WriteLine("Радиус равен " + r);
+
+            Console.ReadKey(true);
+        }
+    }
+}
