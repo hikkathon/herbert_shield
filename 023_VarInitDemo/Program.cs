29dc0de66db3f467680827e5cba42a32006ae59d
hikkathon <hikkathon@gmail.com>
2020-04-03 02:28:26 +0700

    up

--- 023_VarInitDemo/Program.cs
+++ 023_VarInitDemo/Program.cs
@@ -10,7 +10,18 @@ namespace _023_VarInitDemo
     {
         static void Main(string[] args)
         {
+            int x;
 
+            for(x=0; x < 3; x++)
+            {
+                int y = -1; // Переменная y инициализируется при каждом входе в блок.
+                Console.WriteLine($"y равно: {y}");
+
+                y = 100;
+                Console.WriteLine($"y теперь равно: {y}");
+            }
+
+            Console.ReadKey(true);
         }
     }
 }
